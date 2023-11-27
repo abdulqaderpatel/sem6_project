8aa19c98e1619a084819dba44787f64de110bc03
abdulqader <abdulqaderpatel2002@gmail.com>
2023-11-27 21:36:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sem6_project/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sem6_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sem6_project.ui.theme.Sem6_projectTheme
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Sem6_projectTheme {
+
+            }
+        }
+    }
+}
