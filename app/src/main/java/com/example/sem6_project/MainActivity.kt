5c7a7b9101d320aa3a3ad79eab612f8634c98e5e
abdulqader <abdulqaderpatel2002@gmail.com>
2023-11-30 12:04:23 +0530

    set font and font styles for app.

--- app/src/main/java/com/example/sem6_project/MainActivity.kt
+++ app/src/main/java/com/example/sem6_project/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.sem6_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sem6_project.ui.theme.Sem6_projectTheme
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,8 +22,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Sem6_projectTheme {
-
+                Column {
+                    Text(text = "Texting time", style = MaterialTheme.typography.displayLarge, fontWeight = FontWeight.Bold)
+                    Text(text = "Texting time", style = MaterialTheme.typography.displayLarge, fontWeight = FontWeight.Normal)
+                    Text(text = "Texting time", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Light)
+                    Text(text = "Texting time", style = MaterialTheme.typography.displayLarge)
+                }
             }
         }
     }
+}
+
+
+@Composable
+fun Login()
+{
+
+}
+
+@Preview
+@Composable
+fun loginpreview()
+{
+    Login()
 }
--- app/src/main/java/com/example/sem6_project/ui/theme/Type.kt
+++ app/src/main/java/com/example/sem6_project/ui/theme/Type.kt
@@ -1,34 +1,40 @@
 package com.example.sem6_project.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.sem6_project.R
 
-// Set of Material typography styles to start with
+object AppFont {
+    val FontFamily = FontFamily(
+        Font(R.font.helvetica),
+        Font(R.font.helvetica_bold,FontWeight.Bold),
+        Font(R.font.helvetica_light,FontWeight.Light),
+        Font(R.font.helvetica_normal,FontWeight.Normal)
+
+    )
+}
+
+private val defaultTypography = Typography()
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = AppFont.FontFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = AppFont.FontFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = AppFont.FontFamily),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = AppFont.FontFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = AppFont.FontFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = AppFont.FontFamily),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = AppFont.FontFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = AppFont.FontFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = AppFont.FontFamily),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = AppFont.FontFamily),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = AppFont.FontFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = AppFont.FontFamily),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = AppFont.FontFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = AppFont.FontFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = AppFont.FontFamily)
 )
